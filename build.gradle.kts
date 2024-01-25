484a0fc7ad35bd8c264a4515ff996f5d9dd2fa18
mshabiola <mshdabiola@gmal.com>
2024-01-25 01:27:16 +0100

    add dependencies and plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,14 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
+
+//    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
+    alias(libs.plugins.firebase.perf) apply false
+    alias(libs.plugins.gms) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.androidx.baselineprofile) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
