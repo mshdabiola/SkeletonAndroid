4594093734e3172254c35e78849a2db274a51ddb
mshabiola <mshdabiola@gmal.com>
2024-02-01 13:29:27 +0100

    change flow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -130,7 +130,7 @@ dependencies {
 baselineProfile {
     // Don't build on every iteration of a full assemble.
     // Instead enable generation directly for the release build variant.
-    automaticGenerationDuringBuild = false
+    automaticGenerationDuringBuild = true
 }
 
 dependencyGuard {
