79b0b5c5a9a8ed5e0245c715cb4904ca2caee5ef
mshdabiola <mshdabiola@gmail.com>
2024-08-26 18:17:05 +0100

    Refactor: Migrate screenshot tests to use Capture composable
    This commit migrates the screenshot tests to use the new `Capture` composable.
    
    The
     `Capture` composable provides a more convenient and flexible way to take screenshots in tests. It also allows for better integration with the preview system.
    
    The following changes were made:
    
    - The old screenshot tests were removed.
    - New screenshot tests were created using the `Capture` composable.
    - The `
    Preview` annotation was added to the new screenshot tests.

---
+++ features/detail/src/screenshotTest/kotlin/com/mshdabiola/detail/DetailScreenScreenshotTests.kt
@@ -0,0 +1,18 @@
+/*
+ *abiola 2023
+ */
+
+package com.mshdabiola.detail
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.mshdabiola.testing.util.Capture
+
+@Preview
+@Composable
+private fun DetailScreenShot() {
+    Capture {
+        DetailScreen()
+
+    }
+}
