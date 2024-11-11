d2900b8292cfdf0ef101b6905fa8e5c2f32f5fd6
mshdabiola <mshdabiola@gmail.com>
2024-11-11 14:11:22 +0100

    feat: Print test APK location and configure badging tasks
    This commit introduces the following changes:
    
    - It enables the printing of the test APK location during the build.
    - It configures Gradle managed devices for testing on virtual devices.
    - It configures badging tasks for variants and introduces badging checks.
    - It updates compile and target SDK versions to 35, aligning the project with Android 15.
    - It adds the printApks functionality for debug builds.
    - It introduces the capability to generate and check app badging information.
    - It adds the 'badging' command using aapt2 to dump badging information.
    - It prints the location of the test APK if androidTest sources exist.
    - It configures the badging task for various build configurations.

---
+++ build-logic/convention/src/main/kotlin/app/BuildType.kt
@@ -0,0 +1,8 @@
+package app
+
+@Suppress("unused")
+enum class BuildType(val applicationIdSuffix: String? = null, val versionNameSuffix: String = "") {
+    DEBUG(".debug", "-debug"),
+    RELEASE,
+    BENCHMARK(".benchmark", "-benchmark")
+}
